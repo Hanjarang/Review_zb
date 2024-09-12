b676f2b35c2a51c2600e295c24d37332e94788b3
hanjarang <rkdsksehowl@naver.com>
2024-09-12 18:33:21 +0900

    영화 검색 기능 구현 1차 / 영화 전체 목록 조회

--- src/main/java/com/review/repository/MovieRepository.java
+++ src/main/java/com/review/repository/MovieRepository.java
@@ -1,10 +1,19 @@
 package com.review.repository;
 
 import com.review.entity.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
     Optional<Movie> findByTitleAndReleaseYear(String title, int releaseYear);
+
+    // 장편/단편 구분 및 제목의 첫 글자가 한글 또는 영어로 시작하는 영화 목록을 페이지 형식으로 가져옴
+    @Query("SELECT m FROM movies m WHERE m.category = :category AND " +
+            "((:isKorean = true AND SUBSTRING(m.title, 1, 1) BETWEEN '가' AND '힣') OR " +
+            " (:isKorean = false AND SUBSTRING(m.title, 1, 1) BETWEEN 'A' AND 'Z'))")
+    Page<Movie> findByCategoryAndTitleMatchingFirstLetter(String category, boolean isKorean, Pageable pageable);
 }
